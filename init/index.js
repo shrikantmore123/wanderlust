6bde78e08085ef673d837895912c1cb1c8b6ce21
Shrikant More <shrikantmore008@gmail.com>
2025-09-28 15:59:37 +0530

    Implemented Authentication for Login and SignUp

--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj,  owner: "68d8d9a44f8769f1489057e2"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
 }
